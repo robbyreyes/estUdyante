c6e3c04ec87bdfcdc738e4423a9708ffd8544b43
Patrick Panganiban <Ppanganiban68@gmail.com>
2017-10-16 23:47:56 +0800

    modified search

--- application/views/estUdyante/search.php
+++ application/views/estUdyante/search.php
@@ -1,9 +1,11 @@
 <div class="container" id="cont">
 <?php
-if($res!=null)
-{
 
-  foreach($res as $p){?>
+if($res!=null)
+{?>
+Search Results for <?php echo ' "'.$keyword.'"'?> <br />
+<?php foreach($res as $p){?>
+
 
 <a href="<?php echo base_url('profile/id/').$p['id']?>"><?php echo $p['firstname'].' '.$p['lastname']?></a> <br /><br />
 
